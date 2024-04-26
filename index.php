01441968e2c1598ea1a7fcefbc6ac27642001eeb
jnagalakshmi <137597675+jnagalakshmi@users.noreply.github.com>
2024-04-26 18:58:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ error_reporting(E_ALL);
 include 'connection.php';
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-   // username and password sent from form 
+  
    //session_start(); //Start the session
    $username = mysqli_real_escape_string($conn,$_POST['username']);
    $password = mysqli_real_escape_string($conn,$_POST['password']); 
@@ -46,10 +46,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
    $count = mysqli_num_rows($result);
    if($count == 1) {
       //$_SESSION['username']=$username;
-      // ob_start();
-      // header("Location:admin.php");
-      // ob_end_flush();
-       echo "login success";
+       ob_start();
+       header("Location:admin.php");
+       ob_end_flush();
+      
    }else {
       echo "Your Login Name or Password is invalid";
    }
