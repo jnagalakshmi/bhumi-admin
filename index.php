6afabbd49e4242229f13aa1f82a52ca14dc738c4
jnagalakshmi <137597675+jnagalakshmi@users.noreply.github.com>
2024-04-27 12:04:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <div class="main">
 <div class="container">
 <div class="heading">Sign In</div>
-<form class="form" action="index.php" method="post">
+<form class="form" action="login.php" method="post">
 <input
                     placeholder="Username"
                     id="username"
@@ -36,41 +36,4 @@
 </div> 
 </body>
 </html>
-
-<?php
-// Enable error reporting
-ini_set('display_errors',  1);
-ini_set('display_startup_errors',  1);
-error_reporting(E_ALL);
- 
-// Include connection.php
-include 'connection.php';
- 
-// Check if the form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get username and password from the form
-    $username = mysqli_real_escape_string($conn,$_POST['username']);
-    $password = mysqli_real_escape_string($conn,$_POST['password']);
- 
-    // Query to check if the username and password exist in the database
-    $sql = "SELECT * FROM admin WHERE username = '$username' and password = '$password'";
-    $result = mysqli_query($conn,$sql);
-    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-    $count = mysqli_num_rows($result);
- 
-    // If user exists, set session and redirect to home page
-     if($count == 1) {
-      // echo"Welcome";
-      session_start();
-      $_SESSION['username']=$username;
-      //echo "$username";
-    
-      header("Location: ../admin.php");
-   } else {
-        echo "Your Login Name or Password is invalid";
-    }
-}
-?>
- 
-
 
