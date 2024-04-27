0cae7b2456d067b0f1e1f986ce97229bcbfac223
jnagalakshmi <137597675+jnagalakshmi@users.noreply.github.com>
2024-04-27 11:16:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Log In</title>
-<link rel="stylesheet" href="login1.css">
+<link rel="stylesheet" href="login.css">
 </head>
 <body>
 <div class="main">
