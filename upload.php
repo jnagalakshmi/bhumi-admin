c4291d56417626ffabbf000633eaaa18d31fe487
jnagalakshmi <137597675+jnagalakshmi@users.noreply.github.com>
2024-04-27 22:07:10 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -9,7 +9,7 @@ if(isset($_POST["submit"])){
   $category = $_POST["category"];
 
   //For uploads photos
-  $upload_dir = ""; //this is where the uploaded photo stored
+  $upload_dir = "https://github.com/jnagalakshmi/project/blob/main/"; //this is where the uploaded photo stored
   $product_image = $upload_dir.$_FILES["imageUpload"]["name"];
   $upload_dir.$_FILES["imageUpload"]["name"];
   $upload_file = $upload_dir.basename($_FILES["imageUpload"]["name"]);
